4b7c8edaa9cacd862d81fab458641fcdf46e6b39
MuhammedNadeer <muhammednadeerpc@gmail.com>
2024-03-07 20:17:32 +0530

    file upload

--- coviplus-frontend/coviplus-react/src/pages/Predict.jsx
+++ coviplus-frontend/coviplus-react/src/pages/Predict.jsx
@@ -6,9 +6,8 @@ import axios from 'axios';
 const Predict = () => {
     const [imageUrl, setImageUrl] = useState(null);
     const [selectedFile, setSelectedFile] = useState(null);
-    const [prediction, setPrediction] = useState(null);
-    const [explanations, setExplanations] = useState(null);
-    const [error, setError] = useState(null);
+    const [prediction, setPrediction] = useState("");
+    
 
 
     const handleFileChange = (event) => {
@@ -18,46 +17,48 @@ const Predict = () => {
       };
 
       const formData = new FormData();
-      formData.append('image', selectedFile);
+      formData.append('file', selectedFile);
 
     const handleFileUpload = async () => {
-      setError(null);
-      setPrediction(null);
-      setExplanations(null);
 
       if (!selectedFile) {
         setError('Please select an image file.');
         return;
       }
-      try {
-        const response = await fetch("http://127.0.0.1:5000/predict",{
-          method : "POST",
-          body : formData
+      
+        axios.post('http://127.0.0.1:5000/predict', formData)
+        .then(response => {
+          console.log(response.data.result);
+          setPrediction(response.data.result)
+          // Handle response
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          // Handle error
         });
-  
-        setPrediction(response.data.prediction);
-        setExplanations(response.data.explanations);
-      } catch (error) {
-        console.error(error);
-        setError('An error occurred while sending the image.');
-      }
+        // console.log(response)
+        // const data = response.data
+        // console.log(data.result)
+        // setPrediction(data.result);
+        // setExplanations(response.data.probability);
+      
     }
       
       
     return (
         <div className="bg-gray-100 min-h-screen p-16 flex justify-center items-center">
-       <div className="w-2/3 bg-white p-8 rounded-lg shadow-lg">
+       <div className="w-1/2 bg-white p-8 rounded-lg shadow-lg">
          <h1 className="text-2xl font-bold mb-8">Upload Image</h1>
          <div className="h-32 w-32 flex items-center justify-center mb-4 bg-green-200 rounded-lg overflow-hidden">
-           {imageUrl ? <img src={imageUrl} alt="Uploaded Graphic" className="h-full w-auto" /> : <img src={img} alt="Upload Graphic" className="h-full w-auto" />}
+           {imageUrl ? <img src={imageUrl} name="file" alt="Uploaded Graphic" className="h-full w-auto" /> : <img src={img} alt="Upload Graphic" className="h-full w-auto" />}
          </div>
          <input type="file" className="mb-4"  onChange={handleFileChange}/>
          <button className="bg-teal-500 text-white py-2 px-4 rounded w-full" onClick={handleFileUpload}>Upload</button>
        </div>
-       <div className="w-1/3 bg-white p-8 rounded-lg shadow-lg ml-8">
+       <div className="w-1/2 bg-white p-8 rounded-lg shadow-lg ml-8">
          <h1 className="text-2xl font-bold mb-4">Prediction</h1>
          <p className="text-gray-900 mb-4">{prediction}</p>
-         <p className="text-gray-500">{explanations}</p>
+         <p className="text-gray-500"></p>
        </div>
      </div>
     )
